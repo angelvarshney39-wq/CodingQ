fa635c50334815eb922eddfbc3947522db593f0c
angelvarshney39-wq <angelvarshney39@gmail.com>
2026-04-06 19:22:15 +0530

    Update vite.config.js

--- hospital/hospital-main/vite.config.js
+++ hospital/hospital-main/vite.config.js
@@ -2,10 +2,4 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    port: 3000,
-    open: true,
-  },
-});
+
